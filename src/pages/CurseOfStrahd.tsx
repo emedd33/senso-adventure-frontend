3a129ae670434dabf9fb216e6f5d80beb1860ec4
Eskild Mageli <eskild.emedd33@gmail.com>
2020-12-28 12:32:58 +0100

    clean up

--- src/pages/CurseOfStrahd.tsx
+++ src/pages/CurseOfStrahd.tsx
@@ -6,6 +6,7 @@ function CurseOfStrahd() {
     return (
         <Container>
             <Image src={Background} alt="logo" />
+            <Title>Curse of Strahd</Title>
             <StoryContainer>
                 <p>
                     asdasd
@@ -38,12 +39,20 @@ position: sticky;
 top: 5rem;
 z-index:10;
 `
+const Title = styled.h1`
+position: absolute
+top: -50rem;
+margin-top: -60rem;
+left:50vh;
+z-index:20;
+`
 const StoryContainer = styled.div`
-z-index:100;
-width:80rem;
-height: 20rem;
-margin:10rem;
-background-image: url(${OldPaperBackground})
+z-index:20;
+width:80%;
+height: 30rem;
+margin:5rem;
+background-image: url(${OldPaperBackground});
+box-shadow: 5px 0px 15px 2px #000000;
 `
 
 const Container = styled.div`
