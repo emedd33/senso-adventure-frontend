66cc7c3bf866a9e11319ef4132ccb3361395f420
Eskild Mageli <eskild.emedd33@gmail.com>
2020-12-29 18:21:04 +0100

    remove some stuff

--- src/pages/CurseOfStrahd.tsx
+++ src/pages/CurseOfStrahd.tsx
@@ -7,6 +7,9 @@ function CurseOfStrahd() {
         <Container>
             <Image src={Background} alt="logo" />
             <Title>Curse of Strahd</Title>
+            <div style={{backgroundColor:"red"}}>
+                <h1>hehe</h1>
+            </div>
             <StoryContainer>
                 <StoryContent>
                     asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
@@ -16,21 +19,24 @@ function CurseOfStrahd() {
                 </StoryContent>
             </StoryContainer>
             <StoryContainer>
-                <p>
-                    asdasd
-                </p>
-            </StoryContainer>
-            <StoryContainer>
-                <p>
-                    asdasd
-                </p>
-            </StoryContainer>
-            <StoryContainer>
-                <p>
-                    asdasd
-                </p>
-            </StoryContainer>
+                <StoryContent>
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
 
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    asdasdaasdfasdfasdfasdf asdfa sdf asdf asd fas dfasdf asd as d as da sd as da s da sd as d asd a sd a sd a sd as da sd
+                    
+                </StoryContent>
+            </StoryContainer>
+            
         </Container>
     )
 
@@ -42,11 +48,6 @@ position: sticky;
 top: 5rem;
 width:250vh;
 z-index:10;
-
-`
-const StoryContent = styled.p`
-font-size: vmax;
-height:100%;
 `
 const Title = styled.h1`
 position: absolute
@@ -59,13 +60,16 @@ z-index:20;
 `
 const StoryContainer = styled.div`
 z-index:20;
-width:80%;
-height: 50rem;
+min-height: 50rem;
 margin:5rem;
 padding:11rem;
 background-image: url(${OldPaperBackground});
 background-repeat: no-repeat;
-background-size: 100% 100vh;
+background-size: 100% auto;
+`
+//background-size:100% 100vh;
+const StoryContent = styled.p`
+font-size: 2rem;
 `
 
 const Container = styled.div`
