dcb0a97b01d1e6a4a101f6c5958c555d7b3fef90
Eskild Mageli <eskild.emedd33@gmail.com>
2021-02-03 16:49:39 +0100

    Add method for changing password

--- src/pages/Profile/ProfileIndex.tsx
+++ src/pages/Profile/ProfileIndex.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import styled from "styled-components"
-import { storage } from "../../firebase"
+import { firebaseAuth, storage } from "../../firebase"
 import { Route } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import IsLoading from "../../components/IsLoading/IsLoading";
 import { OLD_WHITE } from "../../assets/constants/Constants";
-import { dispatchLogout } from "../../store/admin/adminCreator";
+import { dispatchLogout, setError } from "../../store/admin/adminCreator";
+import TextField from '@material-ui/core/TextField';
+import firebase from "firebase";
 
 export interface ProfileIndexProps {
 
@@ -16,7 +18,61 @@ const ProfileIndex: React.FC<ProfileIndexProps> = () => {
     const dispatch = useDispatch()
     const isLoading = useSelector((state: RootReducerProp) => state.admin.isLoading)
     const [imageUrl, setImageUrl] = useState("")
+    const [isChangingPassword, setIsChangingPassword] = useState(false)
+    const [currentPassword, setCurrentPassword] = useState("")
+    const [firstNewPassword, setFirstNewPassword] = useState("")
+    const [secondNewPassword, setSecondNewPassword] = useState("")
+    const username = useSelector((state: RootReducerProp) => state.admin.authUser?.username)
+    const email = useSelector((state: RootReducerProp) => state.admin.authUser?.email)
+    var user = firebaseAuth.currentUser
 
+    const handleChangePassword = () => {
+
+        if (!firstNewPassword || !secondNewPassword) {
+            dispatch(setError("Please Fill out the passwords"))
+            return
+        }
+        if (firstNewPassword !== secondNewPassword) {
+            dispatch(setError("Passwords are not equal"))
+            return
+        }
+        if (firstNewPassword.length < 6) {
+            dispatch(setError("Passwords must be at least 6 characters"))
+            return
+        }
+        if (user) {
+            if (email) {
+                console.log(currentPassword)
+                var credential = firebase.auth.EmailAuthProvider.credential(
+                    email, // references the user's email address
+                    currentPassword
+                );
+                user.reauthenticateWithCredential(credential)
+                    .then(function () {
+                        // User re-authenticated.
+                        if (user) {
+
+                            user.updatePassword(firstNewPassword)
+                                .then(function () {
+                                    dispatch(setError("Password was changed"))
+                                })
+                                .catch(function (error) {
+                                    console.log(
+                                        "An error occurred while changing the password:",
+                                        error
+                                    );
+                                });
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log("Some kinda bug: ", error);
+                        // An error happened.
+                    });
+
+            }
+        }
+
+    }
     useEffect(() => {
         storage.ref('Images/Background/dnd_login.jpg').getDownloadURL().then((url: string) => setImageUrl(url))
     }, [])
@@ -33,7 +89,79 @@ const ProfileIndex: React.FC<ProfileIndexProps> = () => {
         <Container style={{ backgroundImage: "url(" + imageUrl + ")" }}>
             <Route exact path="/profile">
                 <ContentContainer >
-                    <div style={{ backgroundColor: "red", flex: 8, width: "90%" }}>
+                    <h1>My Profile</h1>
+                    <div style={{ flex: 8, width: "90%" }}>
+                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <ProfileRowString>Username: </ProfileRowString>
+                            <p>{username}</p>
+                        </div>
+                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <ProfileRowString>Email: </ProfileRowString>
+                            <p>{email}</p>
+                        </div>
+                        <Button variant="contained" color="primary" onClick={() => setIsChangingPassword(!isChangingPassword)}>
+                            {isChangingPassword ?
+                                "Close window" :
+                                "Change password"
+                            }
+
+                        </Button>
+                        <ChangePasswordContainer style={isChangingPassword ? { height: "20rem", borderStyle: "dotted" } : { height: "0rem", borderStyle: "none" }}>
+                            {isChangingPassword ?
+                                <div
+                                    style={isChangingPassword ? { opacity: 1, padding: "1rem", display: "initial", transition: "200ms" } : { opacity: 0, padding: "0", display: "none", transition: "200ms" }}
+                                >
+                                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: 1 }}>
+
+                                        <TextField
+                                            fullWidth
+                                            id="current password"
+                                            type="password"
+                                            color="secondary"
+                                            label="Current password"
+                                            placeholder="Write your current password"
+                                            margin="normal"
+                                            style={{ width: "90%" }}
+                                            onChange={(event) => setCurrentPassword(event.target.value)}
+                                        />
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: 1 }}>
+
+                                        <TextField
+                                            fullWidth
+                                            id="second password"
+                                            type="password"
+                                            color="secondary"
+                                            label="New password"
+                                            placeholder="Write your new password"
+                                            margin="normal"
+                                            style={{ width: "90%" }}
+                                            onChange={(event) => setFirstNewPassword(event.target.value)}
+                                        />
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: 1 }}>
+
+                                        <TextField
+                                            fullWidth
+                                            id="second password"
+                                            type="password"
+                                            color="secondary"
+                                            label="New password"
+                                            placeholder="Write your new password"
+                                            margin="normal"
+                                            style={{ width: "90%" }}
+                                            onChange={(event) => setSecondNewPassword(event.target.value)}
+                                        />
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flex: 1, margin: "1rem" }}>
+
+                                        <Button variant="contained" color="primary" onClick={() => handleChangePassword()}>Change password</Button>
+                                    </div>
+                                </div>
+                                :
+                                null}
+                        </ChangePasswordContainer>
+                        <h1>heei</h1>
 
                     </div>
                     <div style={{ flex: 1, width: "90%", justifyContent: "center", alignItems: "center", display: "flex" }}>
@@ -41,9 +169,20 @@ const ProfileIndex: React.FC<ProfileIndexProps> = () => {
                     </div>
                 </ContentContainer>
             </Route>
-        </Container>);
+        </Container >);
 }
 
+const ProfileRowString = styled.h2`
+    font-size:2rem;
+`
+const ChangePasswordContainer = styled.div`
+    height:0rem;
+    margin:1rem;
+    transition:200ms;
+    background-color:white;
+    padding:0rem;
+
+`
 const ContentContainer = styled.div`
 min-height:50rem;
 min-width:15rem;
