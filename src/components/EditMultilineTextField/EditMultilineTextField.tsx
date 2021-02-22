4f7cce59e531c9358e143f0ea75e4863c147ec9d
Eskild Mageli <eskild.emedd33@gmail.com>
2021-02-22 09:06:52 +0100

    Add saved icon into cloud saving

--- src/components/EditMultilineTextField/EditMultilineTextField.tsx
+++ src/components/EditMultilineTextField/EditMultilineTextField.tsx
@@ -3,15 +3,18 @@ import Button from '@material-ui/core/Button';
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { storage } from "../../firebase";
-import { setAlertDialog } from "../../store/admin/adminCreator";
+import CircularProgress from '@material-ui/core/CircularProgress';
 import useInterval from "../../store/hooks/useIntervak";
 export interface EditMultilineTextFieldProps {
 
 }
 
 const EditMultilineTextField: React.FC<EditMultilineTextFieldProps> = () => {
+    const [isUploading, setIsUploading] = useState(false)
     const [isBold, setIsBold] = useState(false)
+    const [isItalic, setIsItalic] = useState(false)
     const [text, setText] = useState("this is start")
+    const [editedText, setEditedText] = useState("This is start")
     const selectedCampaign = useSelector(
         (state: RootReducerProp) => state.selected.selectedCampaign
     );
@@ -28,17 +31,32 @@ const EditMultilineTextField: React.FC<EditMultilineTextFieldProps> = () => {
         setIsBold(!isBold)
         document.execCommand('bold')
     }
-    useInterval(() => {    // Your custom logic here 
-        if (text) {
-            var task = storageRef
+    const handleSetIsItalic = () => {
+        setIsItalic(!isItalic)
+        document.execCommand('italic')
+    }
+    function sleep(time: number) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+    useInterval(async () => {    // Your custom logic here 
+        if (editedText && text !== editedText) {
+            console.log("uploading")
+            setIsUploading(true)
+            storageRef
                 .child("SesionStory.html")
                 .putString(text)
+            setText(editedText)
+            sleep(1000).then(() => {
+                setIsUploading(false)
+            });
+
+
         }
     },
-        10000)
+        5000)
     const handleTextChange = (text: any) => {
         console.log(text)
-        setText(text.innerHTML);
+        setEditedText(text.innerHTML);
 
     }
     useEffect(() => {
@@ -51,10 +69,11 @@ const EditMultilineTextField: React.FC<EditMultilineTextFieldProps> = () => {
     }, [text])
 
     return (
-        <Container>
+        <Container id="cont">
+            {isUploading ? <CircularProgress size="1rem" /> : "Saved"}
             <HeaderContainer>
-
-                <Button onClick={handleSetIsBold} variant="contained" size="small" disableElevation={isBold ? false : true}>Bold</Button >
+                <Button onClick={handleSetIsBold} variant="contained" size="small" >Bold</Button >
+                <Button onClick={handleSetIsItalic} variant="contained" size="small" disableElevation={isItalic ? true : false}>Italic</Button >
             </HeaderContainer>
             <EditContainer contentEditable="true" onInput={e => handleTextChange(e.currentTarget)} id={"editContainer"} suppressContentEditableWarning={true}>
                 <div id="edit-multiline-view"></div>
@@ -66,6 +85,8 @@ const EditMultilineTextField: React.FC<EditMultilineTextFieldProps> = () => {
 
 const Container = styled.div`
     background-color:white;
+    width:100%;
+
 `
 const HeaderContainer = styled.div`
 padding:1rem;
